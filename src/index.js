6722631a4ff02bae5b4703a2c0e15feb613f50e5
Acheniche <99502683+Acheniche@users.noreply.github.com>
2023-02-07 17:31:52 +0300

    feet: update index.js

--- src/index.js
+++ src/index.js
@@ -4,28 +4,28 @@ module.exports = function toReadable (number) {
     var numDec = ['','ten','twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
     var numToTwenty = ['', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
 
-    if (num == 0){
+    if (number == 0){
     output = 'zero';
     }
-    else if (num <= 10){
-      output = numToTen[num];
+    else if (number <= 10){
+      output = numToTen[number];
     }
-    else if (num < 100){
-        if(num <20 && num > 10){
-            output = numToTwenty[num%10];
+    else if (number < 100){
+        if(number <20 && number > 10){
+            output = numToTwenty[number%10];
         }
         else{
-            output = numDec[(num - (num%10))/10] + ' ' + numToTen[(num%10)];
+            output = numDec[(number - (number%10))/10] + ' ' + numToTen[(number%10)];
         }
     }
-    else if (num<1000){
-      output = numToTen[((num - (num%100))/100)] + ' hundred ';
-      if ( (num%1000)%100 < 20 && (num%1000)%100 > 10)
+    else if (number<1000){
+      output = numToTen[((number - (number%100))/100)] + ' hundred ';
+      if ( (number%1000)%100 < 20 && (number%1000)%100 > 10)
       {
-        output += numToTwenty[(num%100)%10];
+        output += numToTwenty[(number%100)%10];
       }
       else {
-        output += (numDec[((num%100) - ((num%100)%10))/10] + ' '+ numToTen[((num%100)%10)]).trim();
+        output += (numDec[((number%100) - ((number%100)%10))/10] + ' '+ numToTen[((number%100)%10)]).trim();
       }
     }
     return   output.trim();
