ff33de508ec1ff6af751b5cb6b9c70c74d6822bd
Acheniche <99502683+Acheniche@users.noreply.github.com>
2023-02-07 17:27:22 +0300

    feet: update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,32 @@
 module.exports = function toReadable (number) {
-    var a = ['','one ','two ','three ','four ', 'five ','six ','seven ','eight ','nine ','ten ','eleven ','twelve ','thirteen ','fourteen ','fifteen ','sixteen ','seventeen ','eighteen ','nineteen '];
-  var b = ['', '', 'twenty','thirty','forty','fifty', 'sixty','seventy','eighty','ninety'];
-  
-      if ((number = number.toString()).length > 9) return 'overflow';
-      n = ('000000000' + number).substr(-9).match(/^(\d{2})(\d{2})(\d{2})(\d{1})(\d{2})$/);
-      if (!n) return; var str = '';
-      str += (n[1] != 0) ? (a[Number(n[1])] || b[n[1][0]] + ' ' + a[n[1][1]]) + 'crore ' : '';
-      str += (n[2] != 0) ? (a[Number(n[2])] || b[n[2][0]] + ' ' + a[n[2][1]]) + 'lakh ' : '';
-      str += (n[3] != 0) ? (a[Number(n[3])] || b[n[3][0]] + ' ' + a[n[3][1]]) + 'thousand ' : '';
-      str += (n[4] != 0) ? (a[Number(n[4])] || b[n[4][0]] + ' ' + a[n[4][1]]) + 'hundred ' : '';
-      str += (n[5] != 0) ? ((str != '') ? 'and ' : '') + (a[Number(n[5])] || b[n[5][0]] + ' ' + a[n[5][1]]) + '' : '';
-      return str;
+    var output = '';
+    var numToTen = ['','one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten'];
+    var numDec = ['','ten','twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    var numToTwenty = ['', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+
+    if (num == 0){
+    output = 'zero';
+    }
+    else if (num <= 10){
+      output = numToTen[num];
+    }
+    else if (num < 100){
+        if(num <20 && num > 10){
+            output = numToTwenty[num%10];
+        }
+        else{
+            output = numDec[(num - (num%10))/10] + ' ' + numToTen[(num%10)];
+        }
+    }
+    else if (num<1000){
+      output = numToTen[((num - (num%100))/100)] + ' hundred ';
+      if ( (num%1000)%100 < 20 && (num%1000)%100 > 10)
+      {
+        output += numToTwenty[(num%100)%10];
+      }
+      else {
+        output += (numDec[((num%100) - ((num%100)%10))/10] + ' '+ numToTen[((num%100)%10)]).trim();
+      }
+    }
+    return   output.trim();
   }
